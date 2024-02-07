676a6e1425646d6428bb109fec920b572769a681
FaceRollan <74344381+FaceRollan@users.noreply.github.com>
2024-02-07 10:55:43 -0600

    Update and rename Day1_Data_Types to Day1_Data_Types.py
    Added 2nd attempt with Declare step completed. (both work)

---
+++ Day1_Data_Types.py
@@ -0,0 +1,44 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+integer = int(input())    # \
+dubs = float(input())     #   ---- I didn't know what it meant to declare, so i redid this solution below.
+stringy = str(input())    # /
+
+# Read and save an integer, double, and String to your variables.
+# 
+
+# Print the sum of both integer variables on a new line.
+bothInts = integer + i
+print(bothInts)
+
+# Print the sum of the double variables on a new line.
+print(dubs + d)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + stringy)
+
+# --------------SECOND ATTEMPT----------------#
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+integer = 0
+dubs = 0
+stringy = 0
+# Read and save an integer, double, and String to your variables.
+integer = int(input())
+dubs = float(input())
+stringy = str(input())
+# Print the sum of both integer variables on a new line.
+bothInts = integer + i
+print(bothInts)
+# Print the sum of the double variables on a new line.
+print(dubs + d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + stringy)
+
