ee47baefcfbfdaeb5a781b5413b0fdbb01bde9c4
FaceRollan <74344381+FaceRollan@users.noreply.github.com>
2024-02-07 17:59:37 -0600

    Update Day2_Operators.py
    Added the HackerRank solution

--- Day2_Operators.py
+++ Day2_Operators.py
@@ -30,3 +30,25 @@ if __name__ == '__main__':
     tax_percent = int(input().strip())
 
     solve(meal_cost, tip_percent, tax_percent)
+
+
+# -------- How Someone else Solved ----- #
+def get_total_cost_of_meal():
+    # original meal price
+    meal_cost = float(input())
+    # tip percentage
+    tip_percent = int(input())
+    # tax percentage
+    tax_percent = int(input())
+
+    # Write your calculation code here
+    tip = meal_cost * tip_percent / 100
+    tax = meal_cost * tax_percent / 100
+
+    # cast the result of the rounding operation to an int and save it as total_cost 
+    total_cost = int(round(meal_cost + tax + tip))
+    
+    return str(total_cost)
+
+# Print your result
+print("The total meal cost is " + get_total_cost_of_meal() + " dollars.")
